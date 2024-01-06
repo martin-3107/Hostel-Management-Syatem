1b6718385b63124b8d46eb65c5a8c9c47235bc59
martin-3107 <martinprajapati3107@gmail.com>
2024-01-06 17:34:35 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hostel Management System</h1>
+  <h1>Hostel Management System 2.0</h1>
 );
